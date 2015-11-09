ab5a75703810bf9bf16c6622587879e6b3b89dc4
TondoDev <tondodeveloper@gmail.com>
2015-11-09 22:42:38 +0100

    added example with javascript enabled view.
    used webjars imported with @Grab groovy annotiation. Webjars are jacrs
    packaging some useful javascript libraries. For more dvanced usage see
    Bower

--- groovy-with-view/controller.groovy
+++ groovy-with-view/controller.groovy
@@ -1,3 +1,8 @@
+/**
+ * http://www.webjars.org/
+ * Wrapped JavaScript libraries into Maven or Gradle artifact/
+ */
+@Grab("org.webjars:jquery:2.1.4")
 @Grab("thymeleaf-spring4")
 @Controller
 class ViewBasedApp {
@@ -8,6 +13,14 @@ class ViewBasedApp {
     // thenekeaf needs templates/ forlder on classpath
     return new ModelAndView("home") //templates/home.html, view resolver is configured automatically by boot
       .addObject("name", name) // binding object to names which are used in template
+      .addObject("chapters", chapters)
+  }
+
+  @RequestMapping(value="/withjs")
+  def modern(@RequestParam(value="name", defaultValue="World") String name) {
+
+    return new ModelAndView("modern")
+      .addObject("name", name)
       .addObject("chapters", chapters)
   }
 }
