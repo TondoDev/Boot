a00409a58466eb8c353131883abb0a16412d3cf3
TondoDev <tondodeveloper@gmail.com>
2016-07-03 21:52:27 +0200

    [booklist] created project for booklist

---
+++ booklist/src/main/java/org/tondo/booklist/BookListApplication.java
@@ -0,0 +1,12 @@
+package org.tondo.booklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookListApplication.class, args);
+	}
+}
