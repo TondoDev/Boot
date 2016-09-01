500caa4224c63a208074ef12858e131a01438b08
TondoDev <tondodeveloper@gmail.com>
2016-09-01 22:46:56 +0200

    [boot] used CounterService and GaugeService
    added custom metrics with these two services injected
    into controller

--- booklist/src/main/java/org/tondo/booklist/web/BookController.java
+++ booklist/src/main/java/org/tondo/booklist/web/BookController.java
@@ -3,6 +3,8 @@ package org.tondo.booklist.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.tondo.booklist.domain.Book;
 import org.tondo.booklist.domain.BookRepository;
 
-// enables populationg propetries (which have setters)  in this class with values
+// enables population of properties (which have setters)  in this class with values
 // from property files
 @ConfigurationProperties(prefix = "tondo")
 @Controller
@@ -31,10 +33,20 @@ public class BookController {
 	public BookController(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
 	}
+	
+	@Autowired
+	private CounterService counterService;
+	
+	@Autowired
+	private GaugeService gaugeService;
 
 	
 	@RequestMapping(value = "/{reader}", method = RequestMethod.GET)
 	public String readersBookList(@PathVariable ("reader") String reader, Model model) {
+		// adds one to custom counter accessible from /metrics actuator endpoint
+		// after each time book list service is called
+		// in metrics will be listed as counter.linstcnt
+		counterService.increment("listcnt");
 		
 		List<Book> books = bookRepository.findByReader(reader);
 		if (books != null) {
@@ -50,6 +62,9 @@ public class BookController {
 	public String markForRead(@PathVariable String reader, Book book) {
 		book.setReader(reader);
 		bookRepository.save(book);
+		
+		// stores time in miliseconds when last book was saved
+		gaugeService.submit("lastbooksaved", System.currentTimeMillis());
 		return "redirect:/a/{reader}";
 	}
 }
