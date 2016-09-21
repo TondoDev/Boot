adb21e748aac0071c3699d1ed93861b2dabe8c6b
TondoDev <tondodeveloper@gmail.com>
2016-09-21 19:18:44 +0200

    [boot] application prepared for deploy on external container
    achieved by implementing Servlet initializer

--- booklist/src/main/java/org/tondo/booklist/BookListApplication.java
+++ booklist/src/main/java/org/tondo/booklist/BookListApplication.java
@@ -2,9 +2,16 @@ package org.tondo.booklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookListApplication {
+public class BookListApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BookListApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookListApplication.class, args);
